0ca15fc976f1de9b980c90982a7a5daeedb89b68
andrew_ito <andrew_ito@datahouse.com>
2015-09-18 22:22:59 -1000

    adding async calls

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 var serverUtil = require('./client/services/serverUtilities');
+var async = require('async');
 
+var inputSentance = 'the dog is brown';
+var outputSentance = [];
+async.eachSeries(inputSentance.split(' '), 
+	function(item, eachCallback){
+		serverUtil.send('GET', item, function(error, response, body){
+			outputSentance.push(body);
+			return eachCallback();
+		})
+	},
+	function(error){
+		console.log(outputSentance);
+	});
+
+/*
 serverUtil.send('GET', 'test', function(error, response, body){
 	console.log(error, body);
 })
+*/
