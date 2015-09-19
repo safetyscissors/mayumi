2c47e8e4d41d4b372cfc9ec15d6cabfd16ffd3d6
Cameron Dye <cameronkdye@gmail.com>
2015-09-18 23:10:11 -1000

    Added printOutput, starting chunk processing

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 var serverUtil = require('./client/services/serverUtilities');
+var fs = require('fs');
 
 serverUtil.send('GET', 'test', function(error, response, body){
 	console.log(error, body);
 })
+
+fs.readFile('./testFile', 'utf8', function (error, data) {
+  if(error){
+    return error
+  }
+  console.log(data);
+});
+
+
+function printOutput(partArray) {
+
+	var out = JSON.stringify(partArray);
+	return out;
+}
+
+
+var testNew = {
+	test:"The Dog",
+	part_speech: "subject"
+};
+
+var testNew2 = {
+	test:"ate",
+	part_speech: "verb"
+};
+
+var outStuff = printOutput([testNew, testNew2]);
+console.log(outStuff);
