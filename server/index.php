604a4f4a6b80b9f6f85a88d7ec2c4488492b1b47
andrew_ito <andrew_ito@datahouse.com>
2015-09-18 17:04:44 -1000

    one more array replacement

--- server/index.php
+++ server/index.php
@@ -15,7 +15,7 @@
 	$router->route();
 
 
-	$resul=$dbService->query("INSERT INTO words (word, frequency) VALUES('test',2)", []);
+	$resul=$dbService->query("INSERT INTO words (word, frequency) VALUES('test',2)", array());
 	$resul2=$dbService->query("SELECT * FROM words", array());
 	echo($resul2);
 
